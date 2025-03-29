c9783e6d5e61e812b582276c43270a29920c475b
Wali <humaidwali20@gmail.com>
2025-03-29 16:41:42 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Mines',
+  description: '',
+  generator: '',
+  icons:{
+    icon:'/favicon.ico'
+  }
 }
 
 export default function RootLayout({
